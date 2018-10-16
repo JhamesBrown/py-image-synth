a554602ec06662be12083976d36efb6f32e20986
James <jamestron@gmail.com>
2018-10-16 17:59:59 +1300

    first run

---
+++ image-synth-start.py
@@ -0,0 +1,15 @@
+import random
+from PIL import Image, ImageDraw
+
+img = Image.new('RGB', (200,200), color = (255,0,0))
+
+draw = ImageDraw.Draw(img)
+
+for x in range(0, img.size[1]):
+    rand1 = random.randint(0,img.size[0])
+    rand2 = random.randint(0,img.size[0])
+    draw.line((rand1,x,rand2,x), fill= (0,0,255) )
+del draw
+
+
+img.save('exp_002.png')
